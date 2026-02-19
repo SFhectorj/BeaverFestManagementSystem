cc9972bce3019bafa7b2cf2d91924296c1d7230e
SFhectorj <baezahectorj@gmail.com>
2026-02-19 01:07:49 -0800

    Added forms for all tables including creating new dedicated update files. Updated app.js to properlyy route update button to its corresponding forms.

--- app.js
+++ app.js
@@ -39,13 +39,12 @@ app.get('/bands', function(req, res)
 app.get('/venues', function(req, res)
     {
         // Query 1: Get all Venues (Joined with Managers and Sponsors for readable names)
-        let query1 = "SELECT Venues.venueId, Venues.venueName, Venues.capacity, Venues.type, CONCAT(Managers.firstName, ' ', Managers.lastName) AS managerName, Sponsors.sponsorName FROM Venues LEFT JOIN Managers ON Venues.managerId = Managers.managerId LEFT JOIN Sponsors ON Venues.sponsorId = Sponsors.sponsorId;";
-
+        let query1 = "SELECT Venues.venueID, Venues.venueName, Venues.capacity, Venues.venueType, Venues.locationDescription, CONCAT(Managers.firstName, ' ', Managers.lastName) AS managerName, Sponsors.sponsorName FROM Venues LEFT JOIN Managers ON Venues.managerID = Managers.managerID LEFT JOIN Sponsors ON Venues.sponsorID = Sponsors.sponsorID;";
         // Query 2: Get all Managers for the dropdown
-        let query2 = "SELECT managerId, firstName, lastName FROM Managers;";
+        let query2 = "SELECT managerID, firstName, lastName FROM Managers;";
 
         // Query 3: Get all Sponsors for the dropdown
-        let query3 = "SELECT sponsorId, sponsorName FROM Sponsors;";
+        let query3 = "SELECT sponsorID, sponsorName FROM Sponsors;";
 
         // Run Query 1...
         db.pool.query(query1, function(error, rows, fields){
@@ -80,13 +79,12 @@ app.get('/venues', function(req, res)
 app.get('/performances', function(req, res)
     {
         // Query 1: Get the table data (Performances + readable Names)
-        let query1 = "SELECT Performances.performanceId, Bands.bandName, Venues.venueName, Performances.performanceDate, Performances.startTime, Performances.duration FROM Performances INNER JOIN Bands ON Performances.bandId = Bands.bandId INNER JOIN Venues ON Performances.venueId = Venues.venueId ORDER BY Performances.performanceDate, Performances.startTime;";
-
+        let query1 = "SELECT Performances.performanceID, Bands.bandName, Venues.venueName, Performances.performanceDate, Performances.startTime, Performances.durationMinutes FROM Performances INNER JOIN Bands ON Performances.bandID = Bands.bandID INNER JOIN Venues ON Performances.venueID = Venues.venueID ORDER BY Performances.performanceDate, Performances.startTime;";
         // Query 2: Get all Bands to populate the "Band" dropdown
-        let query2 = "SELECT bandId, bandName FROM Bands;";
+        let query2 = "SELECT bandID, bandName FROM Bands;";
 
         // Query 3: Get all Venues to populate the "Venue" dropdown
-        let query3 = "SELECT venueId, venueName FROM Venues;";
+        let query3 = "SELECT venueID, venueName FROM Venues;";
 
         // Run Query 1...
         db.pool.query(query1, function(error, rows, fields){
@@ -142,13 +140,13 @@ app.get('/sponsors', function(req, res)
 // 7. Route to display the Update Band form
 app.get('/update-band/:id', function(req, res) {
     // Grab the bandId from the URL parameter
-    let bandId = req.params.id;
+    let bandID = req.params.id;
 
     // Query the database for this specific band
-    let query1 = "SELECT * FROM Bands WHERE bandId = ?;";
+    let query1 = "SELECT * FROM Bands WHERE bandID = ?;";
 
-    // We use [bandId] to safely insert the variable into the '?' placeholder
-    db.pool.query(query1, [bandId], function(error, rows, fields) {
+    // We use [bandID] to safely insert the variable into the '?' placeholder
+    db.pool.query(query1, [bandID], function(error, rows, fields) {
         if (error) {
             console.log(error);
             res.sendStatus(400);
@@ -156,6 +154,83 @@ app.get('/update-band/:id', function(req, res) {
             // Render the update-band template, passing it the single row we found
             res.render('update-band', { band: rows[0] });
         }
+    });
+});
+
+// 8. Route to display the Update Venue form
+app.get('/update-venue/:id', function(req, res) {
+    let venueID = req.params.id;
+
+    // Query 1: Get the specific venue we want to update
+    let query1 = "SELECT * FROM Venues WHERE venueID = ?;";
+    // Query 2: Get all managers for the dropdown
+    let query2 = "SELECT managerID, firstName, lastName FROM Managers;";
+    // Query 3: Get all sponsors for the dropdown
+    let query3 = "SELECT sponsorID, sponsorName FROM Sponsors;";
+
+    // Run Query 1
+    db.pool.query(query1, [venueID], function(error, rows, fields) {
+        let venue = rows[0]; // Save the single venue row
+        
+        // Run Query 2
+        db.pool.query(query2, function(error, rows, fields) {
+            let managers = rows; // Save the managers list
+            
+            // Run Query 3
+            db.pool.query(query3, function(error, rows, fields) {
+                let sponsors = rows; // Save the sponsors list
+                
+                // Render the page with all the necessary data
+                res.render('update-venue', {
+                    venue: venue,
+                    managers: managers,
+                    sponsors: sponsors
+                });
+            });
+        });
+    });
+});
+
+
+// 9. Route to display the Update Performance form
+app.get('/update-performance/:id', function(req, res) {
+    let performanceID = req.params.id;
+    let query1 = "SELECT * FROM Performances WHERE performanceID = ?;";
+    let query2 = "SELECT bandID, bandName FROM Bands;";
+    let query3 = "SELECT venueID, venueName FROM Venues;";
+
+    db.pool.query(query1, [performanceID], function(error, rows, fields) {
+        let performance = rows[0];
+        db.pool.query(query2, function(error, rows, fields) {
+            let bands = rows;
+            db.pool.query(query3, function(error, rows, fields) {
+                let venues = rows;
+                res.render('update-performance', {
+                    performance: performance,
+                    bands: bands,
+                    venues: venues
+                });
+            });
+        });
+    });
+});
+
+
+// 10. Route to display the Update Manager form
+app.get('/update-manager/:id', function(req, res) {
+    let managerID = req.params.id;
+    let query1 = "SELECT * FROM Managers WHERE managerID = ?;";
+    db.pool.query(query1, [managerID], function(error, rows, fields) {
+        res.render('update-manager', { manager: rows[0] });
+    });
+});
+
+// 11. Route to display the Update Sponsor form
+app.get('/update-sponsor/:id', function(req, res) {
+    let sponsorId = req.params.id;
+    let query1 = "SELECT * FROM Sponsors WHERE sponsorID = ?;";
+    db.pool.query(query1, [sponsorId], function(error, rows, fields) {
+        res.render('update-sponsor', { sponsor: rows[0] });
     });
 });
 
